b515a95be2e87a9df55d6e082d5808222a7f23b3
Artur Gin <genyceny@gmail.com>
2023-07-15 16:12:02 +0400

    feat: enforce HTTP request to use specified objects

---
+++ lib/request_enforcer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "request_enforcer/version"
+
+module RequestEnforcer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/request_enforcer_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RequestEnforcer do
+  it "has a version number" do
+    expect(RequestEnforcer::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
